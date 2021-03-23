56a3cd96951009707c800ca5954f417c5aa2c078
qinyj12 <qinyj12@gmail.com>
2021-03-23 22:50:08 +0800

    创建user目录保存头像

--- resource/user/avatar.py
+++ resource/user/avatar.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, reqparse, request
 from database import database_tables, database_factory
 from ..token import token_verify
 from werkzeug.datastructures import FileStorage
+import pathlib
 
 app = Blueprint('avatar', __name__, url_prefix = '/avatar')
 api = Api(app)
@@ -26,10 +27,11 @@ class Avatar(Resource):
         # 解析token中的信息
         token_verified = token_verify.verify_token(arg_token)
         user_id = token_verified['id']
-        
-
+        save_dir = './static/user/' + str(user_id)
+        # 创建名为user_id的目录，用来保存头像。如果目录已存在的话，就不用创建了
+        pathlib.Path(save_dir).mkdir(parents=True, exist_ok=True) 
         # 此处要指定保存的位置和文件名
-        arg_avatar.save('demo.png')
+        arg_avatar.save(save_dir + '/avatar.png')
         print(arg_avatar.read())
 
         return {
