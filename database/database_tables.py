e956e31e7c84d7b00a95a9ec216e314acb685ab9
qinyj12 <qinyj12@gmail.com>
2021-04-28 23:58:53 +0800

    用alembic管理数据库版本

--- database/database_tables.py
+++ database/database_tables.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 # 引入Base基类
 from database.database_factory import Base
 
 # User表
 class User(Base):
     __tablename__ = 'User'
-    id = Column(Integer, primary_key = True, nullable = True)
+    id = Column(Integer, primary_key = True, nullable = False)
     name = Column(String(20), nullable = False)
     email = Column(String(20), nullable = True)
     phone = Column(String(20), nullable = True)
@@ -15,6 +16,18 @@ class User(Base):
     roles = Column(String(20), nullable = True)
     introduction = Column(String(100), nullable = True)
     signup_time = Column(DateTime, default = func.now(), nullable = False)
+    relate_article = relationship('Article', backref = 'relate_user', lazy = 'dynamic')
+
+# Article表
+class Article(Base):
+    __tablename__ = 'Article'
+    id = Column(Integer, primary_key = True, nullable = False)
+    title = Column(String(20), nullable = False)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    create_time = Column(DateTime, default = func.now(), nullable = False)
+    cover = Column(String(100), nullable = False)
+    state = Column(String(20), nullable = False)
+    tag = Column(String(20), nullable = True)
 
 # 邮件验证码表
 class Mail_Code(Base):
